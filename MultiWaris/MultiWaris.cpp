ebacd60bea4b34957a3c914f220987412a54636d
fahreziahmad <alfahreziahmadsyahyana@gmail.com>
2024-05-28 19:05:11 +0700

    menambahkan class orang

--- MultiWaris/MultiWaris.cpp
+++ MultiWaris/MultiWaris.cpp
@@ -1,5 +1,14 @@
-// MultiWaris.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+class orang {
+public:
+	int umur;
+
+	orang(int pUmur) :
+		umur(pUmur)
+	{
+		cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+	}
+};
 
