46434a9e86d93d9eecffae1b91e89f7723230fc7
fahreziahmad <alfahreziahmadsyahyana@gmail.com>
2024-05-28 18:55:37 +0700

    add class abstarksi dann privte public

---
+++ abstraksi/abstraksi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class AbstraksiKlas {
+private:
+
+public:
+
+	// method untuk mengisi nilai
+	// private member
+	void setXY(string a, string b) {
+		x = a;
+		y = b;
+	}
+	//menampilkan nilai
+
+};
+
