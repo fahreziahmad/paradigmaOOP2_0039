637578c157efb17bbdf8c1203fc5dcc9e15f92b5
fahreziahmad <alfahreziahmadsyahyana@gmail.com>
2024-05-28 17:48:30 +0700

    add int main obyek lainnya

--- paradigmaOOP2/paradigmaOOP2.cpp
+++ paradigmaOOP2/paradigmaOOP2.cpp
@@ -17,5 +17,22 @@ public:
 };
 
 class lia :public seseorang {
-
+public:
+	void pesan() {
+		cout << "pesan dari lia << endl;"
+	}
 };
+
+int main() {
+	seseorang* obyek;
+	joko a;
+	lia b;
+
+	obyek = &a;
+	obyek->pesan();
+	obyek = &b;
+	obyek->pesan();
+	//a.seseorang::pesan();
+
+	return 0;
+}
